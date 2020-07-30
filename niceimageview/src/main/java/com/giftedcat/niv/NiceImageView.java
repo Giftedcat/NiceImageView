9d8b9590799263e8b37bbabcf620af92a9e919f2
徐诚聪 <951502877@qq.com>
2020-07-30 12:03:39 +0800

    fix: 修复报错

--- niceimageview/src/main/java/com/giftedcat/niv/NiceImageView.java
+++ niceimageview/src/main/java/com/giftedcat/niv/NiceImageView.java
@@ -17,8 +17,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 
-import com.shehuan.niv.R;
-
 public class NiceImageView extends AppCompatImageView {
     private Context context;
 
